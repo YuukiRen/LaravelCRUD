c8d6b12647716dc34b8f7631f545b14cc98cef94
IARI <reinaldoalvin1@gmail.com>
2018-03-01 21:09:23 +0700

    hmm

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,12 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                      <li class="nav-item active">
+                        <a class="nav-link" href="{{ url('/') }}">Home <span class="sr-only">(current)</span></a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/create') }}">Create</a>
+                      </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
